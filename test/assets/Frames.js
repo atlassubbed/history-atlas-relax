43c36f681838a8807a4a67b00642573645bb3943
atlassubbed <atlassubbed@gmail.com>
2018-08-06 20:49:25 -0400

    adds empty asset files for frame and template combinations

---
+++ test/assets/Frames.js
@@ -0,0 +1,18 @@
+// Classification of Frames:
+//    1. Reducibility
+//      1. Irreducible
+//      2. Reducible
+//        1. Stateful
+//        2. Stateless
+//    2. Rank
+//      1. 0
+//      2. 1
+//      3. 2+
+//    3. Composability
+//      1. Blackbox
+//      2. Functional
+//
+// All frames have a reducibility, rank and composability.
+// Not all of the combinations are valid frames.
+
+const Irreducible
---
+++ test/assets/templates.js
@@ -0,0 +1,8 @@
+// Classification of templates:
+//     1. Void
+//     2. Primitive
+//     3. Object
+//
+// Void templates evaluate to the null frame.
+// Primitive templates are sterile.
+
